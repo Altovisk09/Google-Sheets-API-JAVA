a158e9d5633c0f60aea7017c72c4a1668a18d88f
Altovisk <ericaltao13@gmail.com>
2025-09-07 16:00:33 -0300

    chore(config): atualização das configs do Sheets

--- src/main/java/com/crud/Sheets/config/GoogleSheetsConfig.java
+++ src/main/java/com/crud/Sheets/config/GoogleSheetsConfig.java
@@ -4,7 +4,7 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory; // <-- MUDANÇA 1
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
@@ -23,11 +23,18 @@ import java.util.Collections;
 @Configuration
 public class GoogleSheetsConfig {
 
+    @Value("${google.credentials.client-id}")
+    private String clientId;
+
     @Value("${google.credentials.client-email}")
     private String clientEmail;
 
+    @Value("${google.credentials.private-key-id}")
+    private String privateKeyId;
+
     @Value("${google.credentials.private-key}")
     private String privateKey;
+
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
     @Bean
@@ -35,11 +42,15 @@ public class GoogleSheetsConfig {
 
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
+        // Monta o JSON em memória com TODOS os campos que a biblioteca espera
         String credentialsJsonString = String.format(
-                "{\"type\": \"service_account\", \"client_email\": \"%s\", \"private_key\": \"%s\"}",
-                this.clientEmail,
-                this.privateKey
+                "{\"type\": \"service_account\", \"client_id\": \"%s\", \"private_key_id\": \"%s\", \"private_key\": \"%s\", \"client_email\": \"%s\"}",
+                this.clientId,
+                this.privateKeyId,
+                this.privateKey,
+                this.clientEmail
         );
+
         InputStream credentialsStream = new ByteArrayInputStream(credentialsJsonString.getBytes());
 
         GoogleCredentials credential = ServiceAccountCredentials.fromStream(credentialsStream)
